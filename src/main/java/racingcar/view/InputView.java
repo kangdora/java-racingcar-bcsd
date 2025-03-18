b1ad3416eabef80f1c4cf582e3b4d30fab867e25
KangDora <ioprt123@gmail.com>
2025-03-18 20:12:29 +0900

    feat: 출력 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,15 @@
 package racingcar.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     Scanner sc = new Scanner(System.in);
 
-    public String[] inputCarName() {
+    public List<String> inputCarName() {
         System.out.println("차의 이름을 입력해주세요(구분은 ,로)");
-        return sc.nextLine().split(",");
+        return Arrays.stream(sc.nextLine().split(",")).toList();
     }
 
     public int inputRaceCount() {
--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,9 +1,10 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.Map;
 
 public class ResultView {
-    public void printResult(List<String> carNames) {
+    public void printWinner(List<String> carNames) {
         System.out.print("우승자는 ");
 
         for (String car : carNames) {
@@ -12,4 +13,11 @@ public class ResultView {
 
         System.out.print("입니다.");
     }
+
+    public void printResult(List<Map<String, String>> carPos) {
+        for (Map<String, String> carMap : carPos) {
+            System.out.println(carMap.keySet() + " : " + carMap.values());
+        }
+    }
+
 }
